16c9fa845907649533ea5fdabc8b01ffb8f712c7
Neil2V <49854178+Neil2V@users.noreply.github.com>
2023-05-05 00:08:47 -0500

    P1: Api rest

---
+++ web-service/src/test/java/com/springcloud/webservice/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloud.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
