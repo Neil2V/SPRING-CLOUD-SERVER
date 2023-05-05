2c843add4da3536b0e13e8e4677a04e42878ba36
Neil2V <49854178+Neil2V@users.noreply.github.com>
2023-05-05 00:41:55 -0500

    P3: Config web-service.yml

--- web-service/src/main/java/com/springcloud/webservice/WebServiceApplication.java
+++ web-service/src/main/java/com/springcloud/webservice/WebServiceApplication.java
@@ -1,13 +1,27 @@
 package com.springcloud.webservice;
 
+import com.springcloud.webservice.entity.User;
+import com.springcloud.webservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
-public class WebServiceApplication {
+public class WebServiceApplication implements CommandLineRunner {
+
+	@Autowired
+	UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebServiceApplication.class, args);
 	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.save(User.builder().nombre("Neil").build());
+	}
 }
